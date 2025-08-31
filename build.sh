bdee6936d48998fbf40cf6b583f758348a2fe194
Ashutosh <clashutosh04@gmail.com>
2025-08-31 12:45:27 +0530

    Use python3 in build script - Consistent with pip3 approach

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 pip3 install -r requirements.txt
 
 # Run migrations
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Create output directory structure
 mkdir -p staticfiles
